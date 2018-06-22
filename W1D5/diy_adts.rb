3ebeb1fffc8bd8891aa171cf55eeb273d8c56352
Rex Bodoia <rex@bodoia.com>
2018-06-22 07:46:14 -0700

    Implement map class

--- W1D5/diy_adts.rb
+++ W1D5/diy_adts.rb
@@ -33,3 +33,25 @@ class Queue
     @queue.first
   end
 end
+
+class Map
+  def initialize
+    @map = []
+  end
+
+  def set(key, value)
+    @map << [key, value] if @map.none? { |pair| pair.first == key }
+  end
+
+  def get(key)
+    @map.each { |pair| return pair.last if pair.first == key }
+  end
+
+  def delete(key)
+    @map = @map.reject { |pair| pair.first == key }
+  end
+
+  def show
+    @map
+  end
+end
