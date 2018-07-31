c988fb3c7ba3c46d92c347998a006a2a2d4a7850
Rex Bodoia <rex@bodoia.com>
2018-07-31 07:46:51 -0700

    W7D2

---
+++ W7D2/middleware/frontend/todo_redux.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
+import Root from './components/root';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const preloadedState = localStorage.state ?
+    JSON.parse(localStorage.state) : {};
+  // const store = configureStore(preloadedState);
+  const store = applyMiddlewares(store, addLoggingToDispatch);
+  const root = document.getElementById('content');
+  ReactDOM.render(<Root store={store} />, root);
+});
+
+const addLoggingToDispatch = store => next => action => {
+  console.log(store.getState());
+  console.log(action);
+  next(action);
+  console.log(store.getState());
+}
+
+function applyMiddlewares(store, ...middlewares){
+  let dispatch = store.dispatch;
+  middlewares.forEach({
+    dispatch = middleware(store)(dispatch);
+  });
+  return Object.assign({}, store, { dispatch });
+}
